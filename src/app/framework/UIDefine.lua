d82ef8879e045a12e989d59c05c3d23b76af61cf
liuyangkeal <liuyangqq147@qq.com>
2019-07-14 20:42:26 +0800

    shangchuan

--- src/app/framework/UIDefine.lua
+++ src/app/framework/UIDefine.lua
@@ -186,30 +186,30 @@ UIDefine.LayerFlag = {
 -- 	Lose_UI             = { layer = import("app.viewszhandou.GameLose"),		    	flag = UIDefine.LayerFlag.Main.name,	name = "ZhanDou_Lose_UI"    			    },
 -- }
 
--- 足球的ui
+-- -- 足球的ui
 
-UIDefine.ZUQIU_KEY	= {
-	Loading_UI 			= { layer = import("app.viewszuqiu.GameLoading"),		        flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Loading_UI" 			  	    },
-	Start_UI 			= { layer = import("app.viewszuqiu.GameStart"),	    			flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Start_UI"                     },
-	Help_UI 			= { layer = import("app.viewszuqiu.GameHelp"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Help_UI"    			   		},
-	Voice_UI	        = { layer = import("app.viewszuqiu.GameVoiceSet"),	      		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Voice_UI"    			     	},
-	Play_UI             = { layer = import("app.viewszuqiu.GamePlay"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Play_UI"    			   		},
-	Lose_UI             = { layer = import("app.viewszuqiu.GameLose"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Lose_UI"    			    	},
-	Win_UI              = { layer = import("app.viewszuqiu.GameWin"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Win_UI"    			        },
-}
-
-
--- -- 梭哈的ui
--- UIDefine.SUOHA_KEY	= {
--- 	Loading_UI 			= { layer = import("app.viewssuoha.GameLoading"),		        flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Loading_UI" 			  	    },
--- 	Start_UI 			= { layer = import("app.viewssuoha.GameStart"),	    			flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Start_UI"                     },
--- 	Help_UI 			= { layer = import("app.viewssuoha.GameHelp"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Help_UI"    			   		},
--- 	Over_UI             = { layer = import("app.viewssuoha.GameOver"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Over_UI"    			        },
--- 	Voice_UI	        = { layer = import("app.viewssuoha.GameVoiceSet"),	      		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Voice_UI"    			     	},
--- 	Shop_UI	    	    = { layer = import("app.viewssuoha.GameShop"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Shop_UI"    		   		  	},
--- 	Buy_UI	    	    = { layer = import("app.viewssuoha.GameBuy"),		      	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Buy_UI"    		   		  	},
--- 	Play_UI             = { layer = import("app.viewssuoha.GamePlay"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Play_UI"    			   		},
--- 	Rank_UI 			= { layer = import("app.viewssuoha.Leaderboard"),         			flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Rank_UI"				    	},
+-- UIDefine.ZUQIU_KEY	= {
+-- 	Loading_UI 			= { layer = import("app.viewszuqiu.GameLoading"),		        flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Loading_UI" 			  	    },
+-- 	Start_UI 			= { layer = import("app.viewszuqiu.GameStart"),	    			flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Start_UI"                     },
+-- 	Help_UI 			= { layer = import("app.viewszuqiu.GameHelp"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Help_UI"    			   		},
+-- 	Voice_UI	        = { layer = import("app.viewszuqiu.GameVoiceSet"),	      		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Voice_UI"    			     	},
+-- 	Play_UI             = { layer = import("app.viewszuqiu.GamePlay"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Play_UI"    			   		},
+-- 	Lose_UI             = { layer = import("app.viewszuqiu.GameLose"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Lose_UI"    			    	},
+-- 	Win_UI              = { layer = import("app.viewszuqiu.GameWin"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "ZuQiu_Win_UI"    			        },
 -- }
+
+
+-- 梭哈的ui
+UIDefine.SUOHA_KEY	= {
+	Loading_UI 			= { layer = import("app.viewssuoha.GameLoading"),		        flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Loading_UI" 			  	    },
+	Start_UI 			= { layer = import("app.viewssuoha.GameStart"),	    			flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Start_UI"                     },
+	Help_UI 			= { layer = import("app.viewssuoha.GameHelp"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Help_UI"    			   		},
+	Over_UI             = { layer = import("app.viewssuoha.GameOver"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Over_UI"    			        },
+	Voice_UI	        = { layer = import("app.viewssuoha.GameVoiceSet"),	      		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Voice_UI"    			     	},
+	Shop_UI	    	    = { layer = import("app.viewssuoha.GameShop"),		    	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Shop_UI"    		   		  	},
+	Buy_UI	    	    = { layer = import("app.viewssuoha.GameBuy"),		      	    flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Buy_UI"    		   		  	},
+	Play_UI             = { layer = import("app.viewssuoha.GamePlay"),		    		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Play_UI"    			   		},
+	Rank_UI 			= { layer = import("app.viewssuoha.Leaderboard"),         		flag = UIDefine.LayerFlag.Main.name,	name = "SuoHa_Rank_UI"				    	},
+}
 
 rawset(_G,"UIDefine",UIDefine)
