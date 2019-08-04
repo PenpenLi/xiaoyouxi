e281cd36bb871cc4e84a578966c53c7b11677bf0
liuzhiyong645 <616001054@qq.com>
2019-08-04 23:40:04 +0800

    1

--- src/app/EnterApp.lua
+++ src/app/EnterApp.lua
@@ -28,7 +28,7 @@ end
 function EnterApp:create()
 	print(">>>>>EnterApp:create<<<<<<")
 	self:loadAppFile()
-	local scene = require("app.scenes.ZuQiuScene").new()
+	local scene = require("app.scenes.LiKuiScene").new()
 	return scene
 end
 
