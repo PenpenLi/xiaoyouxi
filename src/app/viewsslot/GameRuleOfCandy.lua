983b7b0418a103d7afb7d5392c197c5c4d062440
liuzhiyong645 <616001054@qq.com>
2019-08-26 00:33:38 +0800

    跟新

--- src/app/viewsslot/GameRuleOfCandy.lua
+++ src/app/viewsslot/GameRuleOfCandy.lua
@@ -7,6 +7,9 @@ function GameRuleOfCandy:ctor( param )
 	assert( param," !! param is nil !! ")
 	assert( param.name," !! param.name is nil !! ")
 	GameRuleOfCandy.super.ctor( self,param.name )
+	local layer = cc.LayerColor:create(cc.c4b(0,0,0,150))
+    self:addChild( layer )
+    self._layer = layer
 
 	self:addCsb( "csbslot/newcandy/PayTableLayerNewCandy.csb" )
 
@@ -58,6 +61,8 @@ end
 
 function GameRuleOfCandy:onEnter()
 	GameRuleOfCandy.super.onEnter( self )
+	casecadeFadeInNode( self.root,0.5 )
+	casecadeFadeInNode( self._layer,0.5,150 )
 end
 
 function GameRuleOfCandy:close()
