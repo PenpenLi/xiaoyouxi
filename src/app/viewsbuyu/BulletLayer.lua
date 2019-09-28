b69b6fa8755dc1180690e1e3d297b9df491966e3
liuzhiyong645 <616001054@qq.com>
2019-09-28 14:17:03 +0800

    1

--- src/app/viewsbuyu/BulletLayer.lua
+++ src/app/viewsbuyu/BulletLayer.lua
@@ -200,10 +200,7 @@ function BulletLayer:createBullet(cur_point,targetFish)
 	if self._automaticAttackState == 2 then
 		bullet:bulletMoveTarget( targetFish )
 	end
-	-- -- 疯狂射击,子弹加速
-	-- if self._insaneShootState == 2 then
-	-- 	bullet:setSpeed(true)
-	-- end
+
 end
 
 function BulletLayer:onTouchBegan( touch, event )
@@ -238,114 +235,14 @@ function BulletLayer:onTouchBegan( touch, event )
 	return true
 end
 function BulletLayer:onTouchMoved( touch, event )
-	-- self:unSchedule()
-	
-	-- if self._beganState == 1 then
-	-- 	self:stopAllAction()
-	-- 	self._beganState = 2
-	-- 	dump( self._beganState,"---------------self._beganState = ")
-	-- end
 
-	-- local cur_point = touch:getLocation()
-	-- self:createBullet(cur_point)
-
-	-- self:schedule( function () ------------------为什么这里加
-	-- 	local cur_point = touch:getLocation()
-	-- 	self:createBullet(cur_point)
-	-- 	print("---------------------222222")
-	-- end,0.2)
-	-- print("---------------------3333333")
 end
 
 function BulletLayer:onTouchEnded( touch, event )
 	self:unSchedule()
-	-- local cur_point = touch:getLocation()
-
-	-- local x = cur_point.x - self._bulletStartPoint.x
-	-- local y = cur_point.y - self._bulletStartPoint.y
-	-- local k = math.atan2( y,x )
-	-- local r = 90 - k * 180 / math.pi
-
-	-- -- bullet:setRotation( r )
-	-- self.ImageGun:setRotation( r )
-
-	-- local param = {
-	-- 	index = self._bulletIndex,
-	-- 	level = self._level,
-	-- 	-- startPoint = self._bulletStartPoint,
-	-- 	bulletRotate = r
-	-- }
-	-- local bullet = BulletNode.new( self,param )
-	-- -- local bullet = ccui.ImageView:create( "image/guns/Bullet1_Normal_1_b.png",1 )
-	-- self.BulletNode:addChild( bullet )
-
-
-	-- bullet:setPosition(0,0)
-	-- local hypotenuse = 300
-	-- local radian = 2 * math.pi/360 * r
-	-- local move_x = math.sin( radian ) * hypotenuse
-	-- local move_y = math.cos( radian ) * hypotenuse
-
-	-- local move_by_pos = cc.p( move_x,move_y )
-	-- dump(move_by_pos,"----------------move_by_pos = ")
-	-- local move_by = cc.MoveBy:create( 1,move_by_pos )
-	-- local repeatForever = cc.RepeatForever:create(move_by )
-	-- bullet:runAction( repeatForever )
-	-- schedule( bullet,function()
-	-- 	local pos = cc.p(bullet:getPosition())
-	-- 	local world_pos = bullet:getParent():convertToWorldSpace( pos )
-	-- 	if world_pos.x > display.width or world_pos.x < 0 or world_pos.y < 0 or world_pos.y > display.height then
-	-- 		bullet:removeFromParent()
-	-- 	end
-	-- end,0.02 )
-
-
-
-
-
-
-
-
-
-
-	-- if cur_point.y <= self._bulletStartPoint.y then
-	-- 	return
-	-- end
-
-	-- -- 创建子弹
-	-- -- 优先从缓存中取
-	-- local bullet = nil
-	-- if #self._bulletList > 0 then
-	-- 	bullet = self._bulletList[1]
-	-- 	table.remove( self._bulletList,1 )
-	-- 	print("---------------> 子弹从缓存中创建 ")
-	-- else
-	-- 	print("---------------> 创建新的子弹 ")
-	-- 	bullet = ccui.ImageView:create( "image/guns/Bullet1_Normal_1_b.png",1 )
-	-- 	bullet:retain()	-- 增加引用计数 为了不被删除
-	-- end
-
-	
-	-- 子弹的动作
-	
-	
-	-- -- 计算要移动的终点
-	-- local x1 = display.width
-	-- local y1 = x1 * y / x
-	-- local move_to = cc.MoveTo:create( 5,cc.p( x1,y1 ) )
-	-- local call_remove = cc.CallFunc:create( function()
-	-- 	bullet:removeFromParent()
-	-- 	table.insert( self._bulletList,bullet )
-	-- end )
-	-- local seq = cc.Sequence:create({ move_to,call_remove })
-	-- bullet:runAction( seq )
-	
 
 end
--- -- 获取子弹node
--- function BulletLayer:getBulletNode( ... )
--- 	return self.BulletNode
--- end
+
 -- 加倍
 function BulletLayer:addMultiple()
 	if self._multiple >= 10 then
@@ -370,8 +267,6 @@ function BulletLayer:iceCapped()
 		return
 	end
 	self.ButtonCongelation:loadTexture( "image/particle/lock1.png",1 )
-	-- print("---------------冰封")
-	-- self._gameLayer._fishLayer._fishContainer:pause()
 
 	-- 冰封，找个冰霜图片替代.....下面冰封时间到还有个layer
 	local layer = cc.LayerColor:create( cc.c4b( 0,0,40,50))
@@ -379,7 +274,6 @@ function BulletLayer:iceCapped()
 	-- 冰封下雪粒子
 	local lizi = cc.ParticleSystemQuad:create("image/particle/skin_buyu.plist") 
 	layer:addChild(lizi)
-	-- lizi:setPosition( display.width,display.height )
 
 	local fishLayer = self._gameLayer._fishLayer
 	local fishs = fishLayer._fishContainer:getChildren()
@@ -388,7 +282,6 @@ function BulletLayer:iceCapped()
 		fishs[i]:pause()
 		fishs[i]:stopSchedule()
 	end
-	-- dump( self._gameLayer._fishLayer._fishContainer,"--------------self._gameLayer._fishLayer._fishContainer = ")
 	performWithDelay( fishLayer._fishContainer,function ()
 		layer:removeFromParent()
 		fishs = fishLayer._fishContainer:getChildren()
@@ -396,7 +289,6 @@ function BulletLayer:iceCapped()
 			fishs[i]:resume()
 			fishs[i]:startSchedule()
 		end
-		-- self._gameLayer._fishLayer._fishContainer:resume()
 		fishLayer._iceState = false
 		self.ButtonCongelation:loadTexture( "image/particle/lock0.png",1 )
 		print("---------------------恢复游動")
@@ -415,9 +307,6 @@ function BulletLayer:insaneShoot()
 	end,20)
 end
 
--- function BulletLayer:shop()
--- 	addUIToScene( UIDefine.BUYU_KEY.Shop_UI )
--- end
 
 -- 自动攻击
 function BulletLayer:automaticAttack()
@@ -447,9 +336,6 @@ function BulletLayer:automaticAttack()
 			return
 		end
 
-		dump( self._maxFish,"----------------self._maxFish = ")
-		dump( self._maxFish._hp,"----------------self._maxFish._hp = ")
-		
 		local fish_pos = cc.p( self._maxFish:getPosition())
 		local fish_worldPos = self._maxFish:getParent():convertToWorldSpace( fish_pos )
 		if fish_worldPos.x < 0 or fish_worldPos.y < 0 or fish_worldPos.x > display.width or fish_worldPos.y > display.height  then
