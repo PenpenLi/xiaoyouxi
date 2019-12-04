90f494c0e3be130487e34a7a21c2d9f6b21c8caa
liuyangkeal <liuyangqq147@qq.com>
2019-12-04 22:03:15 +0800

    上传

--- src/app/viewshunzhan/PeopleSolider.lua
+++ src/app/viewshunzhan/PeopleSolider.lua
@@ -16,6 +16,11 @@ end
 
 function PeopleSolider:onEnter()
 	PeopleSolider.super.onEnter( self )
+
+	-- 添加点击
+	TouchNode.extends( self.Icon, function(event)
+		return self:touchPeople( event ) 
+	end )
 end
 
 
@@ -28,6 +33,20 @@ function PeopleSolider:moveToBattleRegion()
 	if self:getPositionX() >= self._gameLayer:getBattleLeftX() then
 		-- 设置状态
 		self:setStatus( self.STATUS.CANFIGHT )
+	end
+end
+
+
+
+
+function PeopleSolider:touchPeople( event )
+	if event.name == "began" then
+		self._startPos = cc.p(event.x,event.y)
+		return true
+	elseif event.name == "moved" then
+		
+	elseif event.name == "ended" then
+		local now_pos = cc.p(event.x,event.y)
 	end
 end
 
